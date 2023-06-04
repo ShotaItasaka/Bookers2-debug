7176aecad35b33539dc02d97c4292c41b26631ba
ShotaItasaka <shotachemistryt@gmail.com>
2023-06-04 16:13:02 +0000

    部分テンプレート編集、top編集

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,24 @@
 class Book < ApplicationRecord
-  has_many :user
-  validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
+  has_one_attached :profile_image
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :body, {presence: true, length: {maximum: 200}}
+
+  def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  root to: "homes#top"
+   devise_for :users
+  get "home/about" => "homes#about", as: "about"
 
-  root :to =>"homes#top"
-  get "home/about"=>"homes#about"
-
-  devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
